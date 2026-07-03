c8aa16ddaa1070818730a9807e9966dfa0877c17
ArthurGrizafis <arthur.grizafis.barbosa@escola.pr.gov.br>
2026-07-03 08:33:23 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -2,37 +2,37 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+   'Robotica',
+    'Para que serve o sensor de ultrassom em um robô?',
+    'Para medir a distância e evitar colisõe'
 )
 
 criaCartao(
-    'Disciplina 2',
+    'Robotica',
+    'Qual a diferença entre um servo motor e um motor de passo?',
+    'O servo motor gira até um ângulo fixo (ex: 180°), e o de passo gira continuamente em pequenos graus precisos.'
+)
+
+criaCartao(
+    'Matematica',
+    'Se uma PG começa em 3 e a razão, qual o proximo termo?',
+    '6'
+)
+
+criaCartao(
+   'Matematica',
+    'Numa função, o que representa o eixo x e o eixo y no grafico',
+    'O eixo X é a entrada (horizontal) e o eixo Y é o resultado (vertical).'
+)
+
+criaCartao(
+    'Disciplina 5',
     'Pergunta 2',
     'Resposta 2'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-
-criaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
-)
-
-criaCartao(
-    'Disciplina 3',
+    'Disciplina 6',
     'Pergunta 3',
     'Resposta 3'
 )
